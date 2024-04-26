31ab12503bf5a58122ab235c0282840d175dae1f
Joe di Stefano <joeydi@okaypl.us>
2024-04-26 09:14:32 -0400

    start on forms app

--- forms/admin.py
+++ forms/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Form
 
-# Register your models here.
+
+class FormAdmin(admin.ModelAdmin):
+    readonly_fields = [
+        "created_by",
+        "created_at",
+        "updated_by",
+        "updated_at",
+        "schema",
+        "uischema",
+    ]
+
+    fields = [
+        "created_by",
+        "created_at",
+        "updated_by",
+        "updated_at",
+        "name",
+        "description",
+        "schema",
+        "uischema",
+    ]
+
+    def save_model(self, request, obj, form, change,):
+        if not obj.created_by:
+            obj.created_by = request.user
+
+        obj.updated_by = request.user
+
+        super().save_model(request, obj, form, change)
+
+
+admin.site.register(Form, FormAdmin)
--- forms/models.py
+++ forms/models.py
@@ -1,8 +1,28 @@
 from django.db import models
+from django.conf import settings
 
 
 class Form(models.Model):
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        related_name='created_by',
+        blank=True,
+        null=True
+    )
+    updated_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        related_name='updated_by',
+        blank=True,
+        null=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField()
-    description = models.TextField()
-    schema = models.JSONField()
-    uischema = models.JSONField()
+    description = models.TextField(blank=True)
+    schema = models.JSONField(blank=True, default=dict)
+    uischema = models.JSONField(blank=True, default=dict)
+
+    def __str__(self) -> str:
+        return self.name
